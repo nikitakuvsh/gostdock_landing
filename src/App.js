97e9c059acd0f745cc257a4b01f8a1b773698352
nikitakuvsh <world_fre@mail.ru>
2025-03-17 17:42:03 +0300

    add how-it-work block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './main.css';
 import Header from './components/Header/Header';
 import MainPage from './components/MainPage/MainPage';
+import HowItWork from './components/HowItWork/HowItWork';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <MainPage />
+      <div className='black-rectangle'>
+        <HowItWork />
+      </div>
     </div>
   );
 }
---
+++ src/components/HowItWork/HowItWork.jsx
@@ -0,0 +1,34 @@
+import './HowItWork.css';
+import createDocumentImage from '../../images/icons/document.svg';
+import connectionDocumentsImage from '../../images/icons/more-documents.svg';
+import refreshDocumentsImage from '../../images/icons/refresh.svg';
+import howItWorkImage from '../../images/pictures/how-it-work-image.svg';
+
+export default function HowItWork(){
+    return (
+        <div className='how-it-work__container'>
+            <h2 className='how-it-work__title'>Как это работает</h2>
+
+            <div className='how-it-work__content'>
+                <div className='how-it-work__list'>
+                    <div className='how-it-work__list-item'>
+                        <img className='how-it-work__list-image' alt='иконка' src={createDocumentImage}/>
+                        <span className='how-it-work__list-text'>Создание компонента</span>
+                    </div>
+                    <div className='how-it-work__list-item'>
+                        <img className='how-it-work__list-image' alt='иконка' src={connectionDocumentsImage}/>
+                        <span className='how-it-work__list-text'>Интеграция компонентов</span>
+                    </div>
+                    <div className='how-it-work__list-item'>
+                        <img className='how-it-work__list-image' alt='иконка' src={refreshDocumentsImage}/>
+                        <span className='how-it-work__list-text'>Автоматическое обновление <br /> компонентов</span>
+                    </div>
+                </div>
+                
+                <div className='how-it-work__image'>
+                    <img className='how-it-work__image-item' alt='Картинка' src={howItWorkImage}/>
+                </div>
+            </div>
+        </div>
+    );
+}
