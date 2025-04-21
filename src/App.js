889806ccf0d1647453dee0d51612a0de916746f3
nikitakuvsh <world_fre@mail.ru>
2025-04-21 21:19:47 +0300

    try deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
-import useSmoothScroll from './smoothScroll';
 import './main.css';
 import Header from './components/Header/Header';
 import MainPage from './components/MainPage/MainPage';
-import HowItWork from './components/HowItWork/HowItWork';
 import Functional from './components/Functional/Functional';
 import { Reviews } from './components/Reviews/Reviews';
 import Advantages from './components/Advantages/Advantages';
